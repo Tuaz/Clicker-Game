71a74d0f602afd9cac11e8c7c15cc4ffd2556fcd
Tuaz <mathuas_r@live.dk>
2015-04-02 21:47:16 +0200

    Updated

--- main.js
+++ main.js
@@ -3,25 +3,38 @@ $(document).ready(function() {
     $("#hero-name").html(heroName);
     
     Game.Enemy();
+    Game.Hero();
+    Game.Update();
     
     $("#enemy").click(function() {
-    Game.enemyHealth --;
-        $("#enemy-health").html(Game.enemyHealth);
-        if (Game.enemyHealth == 0){
-            Game.Enemy();
-        }
+        Game.enemyHealth -= Game.clickpower;
+        $("#enemy-health").html(Game.enemyHealth);      
+    });
 });
-});
+
 
 Game = {};
 
+Game.fps = 30;
+
+Game.Hero = function (){
+    this.heroExp = 0;
+    this.clickpower = 1;
+    
+    $("#hero-exp").html(this.heroExp);
+}
 
 Game.Enemy = function (){
     this.enemyHealth = 10;
-    this.enemyIsAlive = true;
-    $("#enemy-health").html(this.enemyHealth);
-    
-    
+    $("#enemy-health").html(this.enemyHealth); 
 }
 
-
+Game.Update = function(){
+    
+    if (Game.enemyHealth == 0){
+        $("#hero-exp").html(Game.heroExp++);
+        Game.Enemy();
+        }
+    
+    setTimeout(Game.Update, 1000 / Game.fps);
+}
